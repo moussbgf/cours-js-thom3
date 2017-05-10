541a6cec0ba57a7fa183212fa184d1cc55a50d8b
moussbgf DTA <mboughazif@gmail.com>
2017-05-10 10:36:42 +0200

    mise en place de angularJS (index.js)

--- app/index.js
+++ app/index.js
@@ -1,1 +1,19 @@
-console.log("Ca marche !");
+//angular
+import angular from 'angular';
+
+// Création du module app (application) 1ere methode
+//let app = angular.module('app', []);
+// app.run(function () {
+//     console.log('RUN ANGULAR');
+// });
+
+// Création du module app (application) 2eme methode
+angular.module('app', [])
+
+.config(function () {
+    console.log('CONFIG ANGULAR');
+})
+
+.run(function () {
+    console.log('RUN ANGULAR');
+});
